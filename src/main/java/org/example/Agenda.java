d9956020e70ec7a7e02d1f9d67d8eef80b378365
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-08-20 16:40:59 -0300

    Update class Agenda.java with Medico.java

--- src/main/java/org/example/Agenda.java
+++ src/main/java/org/example/Agenda.java
@@ -5,9 +5,13 @@ import java.util.ArrayList;
 public class Agenda {
     private String data;
     private ArrayList<Paciente> pacientes;
+    private Medico medico;
 
-    public Agenda(){
+    public Agenda(Medico medico){
         pacientes = new ArrayList<Paciente>();
+        data = "";
+        this.medico = medico;
+
     }
 
     public void adicionar(Paciente paciente){
@@ -19,7 +23,43 @@ public class Agenda {
             if(paciente.getCpf().equals(cpf)){
                 System.out.println(paciente.getNome()+" removido com sucesso");
                 pacientes.remove(paciente);
+                pacientes.size();
             }
         }
+    }
+
+    public void pesquisar(String cpf){
+        for(int i=0; i<pacientes.size(); i++){
+            if(pacientes.get(i).getCpf().equals(cpf)){
+                System.out.println(pacientes.get(i).getNome());
+                System.out.println(pacientes.get(i).getTelefone());
+                System.out.println(pacientes.get(i).getIdade());
+                i=pacientes.size();
+            }
+        }
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public ArrayList<Paciente> getPacientes() {
+        return pacientes;
+    }
+
+    public void setPacientes(ArrayList<Paciente> pacientes) {
+        this.pacientes = pacientes;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
     }
 }
