77f4d535e8f3d692be193bc2b58095f6f8301a88
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-08-20 16:25:23 -0300

    Update class Agenda.java

--- src/main/java/org/example/Agenda.java
+++ src/main/java/org/example/Agenda.java
@@ -1,4 +1,25 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Agenda {
+    private String data;
+    private ArrayList<Paciente> pacientes;
+
+    public Agenda(){
+        pacientes = new ArrayList<Paciente>();
+    }
+
+    public void adicionar(Paciente paciente){
+        pacientes.add(paciente);
+    }
+
+    public void remover(String cpf){
+        for(Paciente paciente : pacientes){
+            if(paciente.getCpf().equals(cpf)){
+                System.out.println(paciente.getNome()+" removido com sucesso");
+                pacientes.remove(paciente);
+            }
+        }
+    }
 }
